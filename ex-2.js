9ecfa11b8fb72779e55e5b4a7e643a4d81a390fd
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-07-03 11:31:45 +0700

    nested loop exercise 1-3

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,27 @@
 // เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
-   
+function printStar(target) {
+    let result = '';
+    for(let i = 1; i <= target; i++){
+        for(let j = 1; j <= i; j++){
+            result += '*' + '\t'
+        }
+        result += '\n'
+    }
+    return result
 }
+
+console.log(printStar(5));
+
+
+// single loop method
+function printStar2(target){
+
+    let array = []
+    for(let i = 1; i <= target; i++){
+        array.push('* \t'.repeat(i) + '\n')
+    }
+    return array.join('')
+}
+
+console.log(printStar2(5));
+
--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,15 @@
 // เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+function multiplicationTable(target) {
+
+    let result = '';
+
+    for(let i = 1; i <= target; i++){
+        for(let j = 1; j <= 5; j++){
+            result += (j * i) + '\t'
+        }
+        result +='\n';
+    }
+    return result
 }
+
+console.log(multiplicationTable(5));
